e4b569348632d613ab6593454ac2dd3245c15c69
wkgr2009 <34542079+wkgr2009@users.noreply.github.com>
2020-07-14 12:01:26 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,3 +13,5 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+git clone https://github.com/kenzok8/openwrt-packages.git
+git clone https://github.com/kenzok8/small
